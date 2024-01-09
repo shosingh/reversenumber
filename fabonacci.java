57a9d0ade00be3c9b855fa8993210ef9bae55bd6
shosingh <136896344+shosingh@users.noreply.github.com>
2024-01-09 20:47:50 +0530

    Add files via upload

---
+++ fabonacci.java
@@ -0,0 +1,27 @@
+// import java.util.Scanner;
+
+// public class fabonacci {
+//     public static void main(String[] args) {
+//         Scanner sc = new Scanner(System.in);
+//         System.out.print("enter the number : ");
+//         int n = sc.nextInt();
+
+//         int n1 = 0;
+//         int n2 = 1;
+//         int n3;
+
+        
+
+//         for(int i=2;i<=n;i++){
+
+//             n3=n1+n2;
+//             System.out.println(n1);
+//             n1=n2;
+//             n2=n3;
+//         }
+//     sc.close();
+//     }
+// }
+
+
+
